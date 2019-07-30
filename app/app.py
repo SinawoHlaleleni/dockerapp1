9cfe24ab898ebd0771316a16bfed0f7179ef4688
Sinawo Hlaleleni <47171705+SinawoHlaleleni@users.noreply.github.com>
2019-07-30 16:15:17 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,23 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-class Song(object):
- def __init__(self, lyrics):
-        self.lyrics = lyrics
-
-    def sing_me_a_song(self):
-        for line in self.lyrics:
-            print(line)
-
-happy_bday = Song(["Happy birthday to you",
-                   "I don't want to get sued",
-                   "So i'll stop right there"])
-
-bulls_on_parade = Song(["Then rally around the family",
-                        "With pockets full of shells"])
-happy_bday.sing_me_a_song()
-
-bulls_on_parade.sing_me_a_song()
+def hello_worlds():
+ return 'Hi, people!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
